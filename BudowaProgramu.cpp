751e5340a376294ce42421938cffd93967b4ff51
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-04-27 19:25:48 +0200

    Add files via upload

---
+++ BudowaProgramu.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // i - INPUT o - OUTPUT Stream = strumieñ, potok
+					/*
+					INPUT = wejœcie
+					OUTSIDE - na zewn¹trz
+					OUTPUT - wyjœcie
+					*/
+
+using namespace std;					
+					
+main()
+{	
+	//std - skrót od standard
+	cout << "Tekst do wypisania na zewn¹trz.\n xy";
+	
+	//system("pause");zatrzymanie programu
+	getchar();//Czeka na enter.
+	
+}	
