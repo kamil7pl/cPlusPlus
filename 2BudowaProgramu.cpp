44c51df46ece29ca07948a56e40242340a800315
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-05-01 16:54:08 +0200

    Rename BudowaProgramu.cpp to 2BudowaProgramu.cpp

---
+++ 2BudowaProgramu.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // i - INPUT o - OUTPUT Stream = strumień, potok
+					/*
+					INPUT = wejście
+					OUTSIDE - na zewnątrz
+					OUTPUT - wyjście
+					*/
+
+using namespace std;					
+					
+main()
+{	
+	//std - skrót od standard
+	cout << "Tekst do wypisania na zewnątrz.\n xy";
+	
+	//system("pause");zatrzymanie programu
+	getchar();//Czeka na enter.
+	
+}	
