8ec0dcb6ed2a897985bac7be54c08ce61b3561cd
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-05-01 16:47:38 +0200

    Create Zmienne.cpp

---
+++ Zmienne.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	int liczba = 43;
+	
+	liczba = 74;
+	cout << liczba << " ";
+	
+	liczba=140;
+	cout << liczba<< " ";
+	
+	system("pause");
+	
+}
