e89f96dd800006389a68da4d5f4427b24e0926ad
kamil7pl <46973960+kamil7pl@users.noreply.github.com>
2020-05-09 15:50:37 +0200

    Add files via upload

---
+++ wczytywanie_danych_z_wejscia/wczytywanie_danych_z_wejscia.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int a;
+	cin >> a;
+	cout << "Wartoœæ zmiennej a to: " << a << '\n';
+	
+	string imie;
+	string nazwisko;
+	
+	cout << "Podaj imiê: ";
+	cin >> imie;
+	
+	cout << "Podaj nazwisko: ";
+	cin >> nazwisko;
+	
+	
+	cout << "Nazywasz siê " << imie << " " << nazwisko << '\n';
+	
+	system("pause");
+}
